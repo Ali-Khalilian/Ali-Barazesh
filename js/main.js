1c2757e139ee401ce50e45dc73c59e24b70603f4
Ali Khalilian <alikhaliliankh12@gmail.com>
2024-11-23 11:22:01 +0330

    aass

--- js/main.js
+++ js/main.js
@@ -82,57 +82,60 @@ $(document).ready(function () {
   let lastScrollTop = 0;
 
   $hero.on("scroll touchmove", () => {
-      const currentScrollTop = $hero.scrollTop();
-      let blurValue = 
-          parseFloat($HeadTitle.css("filter").replace("blur(", "").replace("px)", "")) || 0;
-  
-      if (currentScrollTop === 0) {
-          blurValue = 0; // Reset blur
-      } else if (currentScrollTop > lastScrollTop && blurValue < 10) {
-          blurValue = Math.min(blurValue + 0.3, 10); // Increase blur
-      } else if (currentScrollTop < lastScrollTop && blurValue > 0) {
-          blurValue = Math.max(blurValue - 0.3, 0); // Decrease blur
-      }
-  
-      // Apply blur with webkit compatibility
-      $HeadTitle.css({
-          "filter": `blur(${blurValue}px)`,
-          "-webkit-filter": `blur(${blurValue}px)`
-      });
+    const currentScrollTop = $hero.scrollTop();
+    let blurValue =
+      parseFloat(
+        $HeadTitle.css("filter").replace("blur(", "").replace("px)", "")
+      ) || 0;
 
-      $HeadBtn.css({
-        "filter": `blur(${blurValue}px)`,
-        "-webkit-filter": `blur(${blurValue}px)`
+    if (currentScrollTop === 0) {
+      blurValue = 0; // Reset blur
+    } else if (currentScrollTop > lastScrollTop && blurValue < 10) {
+      blurValue = Math.min(blurValue + 0.3, 10); // Increase blur
+    } else if (currentScrollTop < lastScrollTop && blurValue > 0) {
+      blurValue = Math.max(blurValue - 0.3, 0); // Decrease blur
+    }
+
+    // Apply blur with webkit compatibility
+    $HeadTitle.css({
+      filter: `blur(${blurValue}px)`,
+      "-webkit-filter": `blur(${blurValue}px)`,
     });
-  
-      $HeadTitleText.css({
-          "filter": `blur(${blurValue}px)`,
-          "-webkit-filter": `blur(${blurValue}px)`
-      });
-      
-      $headImg.css({
-        "filter": `blur(${blurValue}px)`,
-        "-webkit-filter": `blur(${blurValue}px)`
+
+    $HeadBtn.css({
+      filter: `blur(${blurValue}px)`,
+      "-webkit-filter": `blur(${blurValue}px)`,
     });
-      lastScrollTop = currentScrollTop;
+
+    $HeadTitleText.css({
+      filter: `blur(${blurValue}px)`,
+      "-webkit-filter": `blur(${blurValue}px)`,
+    });
+
+    $headImg.css({
+      filter: `blur(${blurValue}px)`,
+      "-webkit-filter": `blur(${blurValue}px)`,
+    });
+    lastScrollTop = currentScrollTop;
   });
 
   $(function () {
     $("#Calender").waypoint(
       function () {
         $(".progress").each(function () {
-          $(this).animate({
+          $(this).animate(
+            {
               width: $(this).attr("aria-valuenow") + "%",
             },
             2000
           );
         });
-  
+
         this.destroy();
-      }, {
+      },
+      {
         offset: "450",
       }
     );
   });
-
 });
